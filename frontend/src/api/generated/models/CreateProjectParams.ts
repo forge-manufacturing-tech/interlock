0d549228df8641851337f98117b894b4add479ce
Nathan Alam <nathanalam99@gmail.com>
2026-01-26 08:14:59 -0500

    Move frontend to separate repository

--- frontend/src/api/generated/models/CreateProjectParams.ts
+++
@@ -1,9 +0,0 @@
-/* generated using openapi-typescript-codegen -- do not edit */
-/* istanbul ignore file */
-/* tslint:disable */
-/* eslint-disable */
-export type CreateProjectParams = {
-    description?: string | null;
-    name: string;
-};
-
